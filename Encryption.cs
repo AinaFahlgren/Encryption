acb7980085dc6030673e6f775f30ed98a5cb6ad5
Aina <ainaf@Aina.com>
2024-02-02 13:09:49 +0100

    first commit

---
+++ Encryption.cs
@@ -0,0 +1,67 @@
+﻿/* Ett krypteringsprogram för att kryptera/avkryptera sitt namn med hjälp av CeasarChiffer: 
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+D E F G H I J K L M N O P Q R S T U V W X Y Z A B C 
+*/
+
+using System;
+using static System.Console;
+
+class CaesarChiffer
+{
+    static void Main()
+    {
+        WriteLine("Please enter your firstname: ");
+        string? myName = ReadLine();
+
+        int shift = 3;
+        string encryptedName = EncryptCaesar(myName, shift);
+
+        WriteLine("Encrypted name: " + encryptedName);
+        WriteLine("For decrypting your name press 1");
+
+
+        if (int.TryParse(ReadLine(), out int keyPress) && keyPress == 1)
+        {
+            Clear();
+            string decryptedName = DecryptCaesar(encryptedName, shift);
+            WriteLine("Decrypted name: " + decryptedName);
+        }
+        else
+        {
+            WriteLine("Exiting the program");
+        }
+    }
+
+    static string EncryptCaesar(string myName, int shift)
+    {
+        char[] encryptedCharName = new char[myName.Length];
+
+        for (int i = 0; i < myName.Length; i++)
+        {
+            char originalCharName = myName[i];
+
+            if (char.IsLetter(originalCharName))
+            {
+                char shiftChar = (char)(originalCharName + shift);
+
+                if ((char.IsUpper(originalCharName) && shiftChar > 'Z') || (char.IsLower(originalCharName) && shiftChar > 'z'))
+                {
+                    shiftChar = (char)(shiftChar - 26);
+                }
+
+                encryptedCharName[i] = shiftChar;
+            }
+            else
+            {
+                encryptedCharName[i] = originalCharName;
+            }
+        }
+
+        return new string(encryptedCharName);
+    }
+
+    static string DecryptCaesar(string myName, int shift)
+    {
+        return EncryptCaesar(myName, -shift);
+    }
+}
